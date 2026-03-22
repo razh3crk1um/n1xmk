8d681808d8a2b09ae35e83a1b4110f1fbfdd882a
razh3crk1um <razh3crk1um@gmail.com>
2026-03-22 19:48:03 +0800

    refactor(net): adjust network configurations

--- system/network.nix
+++ system/network.nix
@@ -17,6 +17,14 @@
 
   programs.ssh.startAgent = true;
 
+  # disable ipv6
+  #boot.kernel.sysctl = {
+  #  "net.ipv6.conf.all.disable_ipv6" = 1;
+  #  "net.ipv6.conf.default.disable_ipv6" = 1;
+  #  #"net.ipv6.conf.lo.disable_ipv6" = 1;
+  #  #"net.ipv6.conf.wlo1.disable_ipv6" = 1; # necessary
+  #};
+
   # network & base pkgs
   networking = {
     hostName = dna.host;
